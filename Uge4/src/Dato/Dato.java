db0fdf2c61e137fc115f64e74c56c194007615c8
DrunkBanana <45033253+DrunkBanana@users.noreply.github.com>
2019-09-27 08:25:27 +0200

    Dato modified public -> private

--- Uge4/src/Dato/Dato.java
+++ Uge4/src/Dato/Dato.java
@@ -1,12 +1,13 @@
 package Dato;
 import java.text.NumberFormat;
 public class Dato {
-    NumberFormat nf = NumberFormat.getInstance();
+    private NumberFormat nf = NumberFormat.getInstance();
     public static void main(String[] args) {
         // nf.setMinimumIntegerDigits
         Dato d1 = new Dato(2015, 9, 8);
         Dato d2 = new Dato(1999,1,2);
         System.out.println(d1.danish());
+        System.out.println(d1.danishText());
         System.out.println(d2.iso());
     }
     private final static String[] arrayMonths = new String[] {"Januar","Februar","Marts","April","Maj","Juni",
@@ -14,14 +15,14 @@ public class Dato {
     private int year;
     private int month;
     private int day;
-    public Dato(int year, int month, int day){
+    private Dato(int year, int month, int day){
         this.year = year;
         this.month = month;
         this.day = day;
     }
-    public String danish(){return day + "/" + month + "-" + year;}
-    public String danishText(){return day + ". " + arrayMonths[month-1] + " " + year;}
-    public String iso(){
+    private String danish(){return day + "/" + month + "-" + year;}
+    private String danishText(){return day + ". " + arrayMonths[month-1] + " " + year;}
+    private String iso(){
         nf.setMinimumIntegerDigits(2);
         return nf.format(year) + "-" + nf.format(month) + "-" + nf.format(day);
     }
