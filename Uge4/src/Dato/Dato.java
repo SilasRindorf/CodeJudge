45f3887ecd5a40565f0e58095c3ca85f4cbe9d5b
DrunkBanana <45033253+DrunkBanana@users.noreply.github.com>
2019-10-02 09:25:27 +0200

    Added Car2

--- Uge4/src/Dato/Dato.java
+++ Uge4/src/Dato/Dato.java
@@ -2,14 +2,14 @@ package Dato;
 import java.text.NumberFormat;
 public class Dato {
     private NumberFormat nf = NumberFormat.getInstance();
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // nf.setMinimumIntegerDigits
         Dato d1 = new Dato(2015, 9, 8);
         Dato d2 = new Dato(1999,1,2);
         System.out.println(d1.danish());
         System.out.println(d1.danishText());
         System.out.println(d2.iso());
-    }
+    }*/
     private final static String[] arrayMonths = new String[] {"Januar","Februar","Marts","April","Maj","Juni",
             "Juli","August","September","Oktober","November","December"};
     private int year;
--- Uge6/src/Car2.java
+++ Uge6/src/Car2.java
@@ -1,43 +1,65 @@
-public class Car {
+public class Car2 {
     private String brand;
     private String licenseplate;
     private String color;
-    public Car(String brand, String licenseplate, String color) {
+    private int regno;
+    private static int count;
+
+    public Car2(String brand, String licenseplate, String color) {
         this.brand = brand;
         this.licenseplate = licenseplate;
         this.color = color;
+        count++;
+        regno = count;
     }
+    public int getRegno(){
+        return regno;
+    }
+    public static int getNextRegno(){
+        return count + 1;
+    }
+
     public String getBrand(String brand) {
         return brand;
     }
-    public void setBrand(String brand){
+
+    public void setBrand(String brand) {
         this.brand = brand;
     }
+
     public String getLicenseplate() {
         return licenseplate;
     }
-    public void setLicenseplate(String l){
+
+    public void setLicenseplate(String l) {
         licenseplate = l;
     }
+
     public String getColor(String color) {
         return color;
     }
-    public void setColor(String color){
+
+    public void setColor(String color) {
         this.color = color;
     }
+
     public String toString() {
-        return '[' + licenseplate + ']' + ' '  + color + ' ' + brand;
+        return '[' + licenseplate + ']' + ' ' + color + ' ' + brand;
     }
-    public boolean equals(Car c1) {
+
+    public boolean equals(Car2 c1) {
         return c1.licenseplate.equals(licenseplate);
     }
-    public boolean alike(Car c2) {
+
+    public boolean alike(Car2 c2) {
         return brand.equals(c2.brand) && color.equals(c2.color);
     }
-    public String getBrand(){
+
+    public String getBrand() {
         return brand;
     }
-    public String getColor(){
+
+    public String getColor() {
         return color;
     }
 }
