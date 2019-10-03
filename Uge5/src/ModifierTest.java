ca5ca7a239d7ee70ea724d63ec67cbe8948632e5
DrunkBanana <45033253+DrunkBanana@users.noreply.github.com>
2019-10-03 09:26:20 +0200

    Finished Modifier

---
+++ Uge5/src/ModifierTest.java
@@ -0,0 +1,11 @@
+class ModifierTest {
+    private String string1;
+    protected String string2;
+    String string3;
+    public String string4;
+    static String string5;
+    final String string6 = "";
+    public ModifierTest(){
+
+    }
+}
