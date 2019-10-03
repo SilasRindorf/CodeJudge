486ab4732d85f5229629e30cac799e2c311d412b
DrunkBanana <45033253+DrunkBanana@users.noreply.github.com>
2019-10-03 07:57:04 +0200

    Bug fix

--- Uge6/src/Histogram.java
+++ Uge6/src/Histogram.java
@@ -5,7 +5,7 @@ public class Histogram {
         String[] input = scan.nextLine().split(" ");
         String[] check = {"1  - 10  | ", "11  - 20  | ", "21  - 30  | ", "31  - 40  | ", "41  - 50  | ", "51  - 60  | ", "61  - 70  | ", "71  - 80  | ", "81  - 90  | ", "91  - 100 | "};
         for (String s : input) {
-            check[(Integer.parseInt(s) / 11)] += '*';
+            check[(int)(Double.parseDouble(s) / 10.0000000000001)] += '*';
         }
         for (String s : check) {
             System.out.println(s);
